2db3305b1b23472894363e523f793930781b73d9
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-10 15:55:57 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { debounce } from 'debounce';
+import { getRefs } from './helpers/refs';
 import { fetchCountries } from './service/fetchCountries';
+import { getMarkup } from './template/markupCardCountry';
+import { getCartMarkup } from './template/countriesMarkup';
 import './css/styles.css';
 
+const { inputEl, listEl } = getRefs();
+
 const DEBOUNCE_DELAY = 300;
-const inputEl = document.querySelector('#search-box');
-const listEl = document.querySelector('.country-list');
-const countryCardEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(textValue, DEBOUNCE_DELAY));
 
@@ -25,53 +27,11 @@ function textValue(e) {
         );
       }
       if (1 < countries.length && countries.length <= 10) {
-        listEl.innerHTML = '';
         return getMarkup(countries);
       }
       if (1 === countries.length) {
-        return getCartMarkup(countries);
+        getCartMarkup(countries[0]);
       }
     })
     .catch(error => Notify.failure('Oops, there is no country with that name'));
 }
-
-function getMarkup(countries) {
-  // listEl.classList.add('list-style: none')
-  const markup = countries
-    .map(
-      ({ flags, name }) => /*html*/ `
-       <li>
-        <img src="${flags.svg}" alt="Flag: ${name.official}">
-        <p>${name.common}</p>
-      </li>
-  `
-    )
-    .sort((a, b) => a.localeCompare(b))
-    .join('');
-  listEl.innerHTML = markup;
-}
-
-function getCartMarkup(country) {
-  const { capital, name, population, flags, languages } = country;
-  countryCardEl.innerHTML = /*html*/ `
-  <img src="${flags.svg}" alt="Flag: ${name.common}">
-    <h2>${name.common}</h2>
-    <ul>
-      <li>
-        <span>Capital: </span>${capital}
-      </li>
-      <li>
-        <span>Population: </span>${population}
-      </li>
-      <li>
-        <span>Languages: </span>${languages}
-      </li>
-    </ul>
-  `;
-}
-
-// name.official - полное имя страны
-// capital - столица
-// population - население
-// flags.svg - ссылка на изображение флага
-// languages - массив языков
